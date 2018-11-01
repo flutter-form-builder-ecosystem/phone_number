52afbd2a48e4d914d9c13a8e671879f9bc0b5134
Julien Vignali <julien.vignali@secutix.com>
2018-11-01 08:20:03 +0100

    Make parse method available in plugin

--- lib/phone_number.dart
+++ lib/phone_number.dart
@@ -3,11 +3,11 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class PhoneNumber {
-  static const MethodChannel _channel =
-      const MethodChannel('phone_number');
+  static const MethodChannel _channel = const MethodChannel('phone_number');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<dynamic> parse(String string, {String region}) async {
+    final args = {"string": string, "region": region};
+    final result = await _channel.invokeMethod("parse", args);
+    return result;
   }
 }
