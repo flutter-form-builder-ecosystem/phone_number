cf7f0eca9ee370a8edf99350f52f3cdda3879c39
Julien Vignali <github@julienvignali.com>
2019-11-26 19:06:36 +0100

    Update channel name to com.julienvignali/phone_number

--- lib/src/phone_number.dart
+++ lib/src/phone_number.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class PhoneNumber {
-  static const MethodChannel _channel = const MethodChannel('phone_number');
+  static const _channel = MethodChannel('com.julienvignali/phone_number');
 
   static Future<dynamic> parse(String string, {String region}) async {
     final args = {"string": string, "region": region};
