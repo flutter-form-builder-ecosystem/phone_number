e4d818345a29a706007c5393e93811bf6efa85ce
Julien Vignali <julien@julienvignali.com>
2020-10-11 14:45:12 +0200

    Update iOS and Android

--- ios/Classes/PhoneNumberPlugin.m
+++ ios/Classes/PhoneNumberPlugin.m
@@ -1,8 +1,10 @@
 #import "PhoneNumberPlugin.h"
-
 #if __has_include(<phone_number/phone_number-Swift.h>)
 #import <phone_number/phone_number-Swift.h>
 #else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
 #import "phone_number-Swift.h"
 #endif
 
